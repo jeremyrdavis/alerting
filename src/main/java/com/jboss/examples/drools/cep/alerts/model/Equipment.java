f9e724d6cb1ba2d4e2bfdebcadc6d86acfaac8a4
Jeremy Davis <jeremyrdavis@gmail.com>
2011-08-02 12:23:02 -0400

    Added Equipment class and mapping.  Verified w/ test.

---
+++ src/main/java/com/jboss/examples/drools/cep/alerts/model/Equipment.java
@@ -0,0 +1,40 @@
+package com.jboss.examples.drools.cep.alerts.model;
+
+public class Equipment {
+
+	private int id;
+	private int equipmentId;
+	private String deviceName;
+	
+	public Equipment() {
+		super();
+	}
+
+	//--------------------------------------------------------------------------
+	//	generated
+	//--------------------------------------------------------------------------
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getEquipmentId() {
+		return equipmentId;
+	}
+
+	public void setEquipmentId(int equipmentId) {
+		this.equipmentId = equipmentId;
+	}
+
+	public String getDeviceName() {
+		return deviceName;
+	}
+
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+
+}
