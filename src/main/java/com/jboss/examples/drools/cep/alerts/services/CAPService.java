d55ef92fef267d92883ebc4452453c2160c26af3
Jeremy Davis <jeremyrdavis@gmail.com>
2011-09-21 20:44:16 -0400

    cleanup

--- src/main/java/com/jboss/examples/drools/cep/alerts/services/CAPService.java
+++ src/main/java/com/jboss/examples/drools/cep/alerts/services/CAPService.java
@@ -6,10 +6,10 @@ import com.jboss.examples.drools.cep.alerts.model.DerivedAlert;
 public class CAPService {
 	
 	public static void sendCAPAlert(Alert alert){
-		System.out.println("CAPAlert");
+		System.out.println("CAPAlert Single");
 	}
 
-	public static void sendCAPAlert(DerivedAlert alert){
-		System.out.println("CAPAlert");
+	public static void sendMultipleCAPAlert(DerivedAlert alert){
+		System.out.println("CAPAlert Multiple");
 	}
 }
